816c967bf5144f35d16e44a431bab162671f8d2c
shafiul <riddlersajib@gmail.com>
2024-06-01 04:39:36 +0600

    Added reusable widget

--- lib/pages/auth/sign_up.dart
+++ lib/pages/auth/sign_up.dart
@@ -165,13 +165,13 @@ class Signup extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
 
-                      socialLoginButton(
+                      SocialButton(
                         imagePath: 'assets/images/facebook.png',
                       ),
 
                       14.w,
 
-                      socialLoginButton(
+                      SocialButton(
                         imagePath: 'assets/images/google.png',
                       ),
                     ],
@@ -203,39 +203,6 @@ class Signup extends StatelessWidget {
                 ],
               ),
             ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget socialLoginButton({required String imagePath}) {
-
-    return Container(
-      width: 56,
-      height: 56,
-      child: IconButton(
-        onPressed: () {},
-        padding: EdgeInsets.zero,
-        icon: Container(
-          width: 56,
-          height: 56,
-          padding: EdgeInsets.all(16),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            color: Color(0xFFFFFFFF),
-            boxShadow: [
-              BoxShadow(
-                color: Color(0x1A395AB8),
-                offset: Offset(0, 3),
-                blurRadius: 2,
-                spreadRadius: 2,
-              ),
-            ],
-          ),
-          child: Image.asset(imagePath,
-            height: 30,
-            width: 30,
           ),
         ),
       ),
