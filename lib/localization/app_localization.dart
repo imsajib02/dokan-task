693c492afbc472c258d593ddf811e059fe1ba5ae
shafiul <riddlersajib@gmail.com>
2024-06-01 00:55:29 +0600

    App localization provider

---
+++ lib/localization/app_localization.dart
@@ -0,0 +1,15 @@
+import 'package:get/get.dart';
+
+class AppLocalization extends Translations {
+
+  @override
+  Map<String, Map<String, String>> get keys => {
+
+    'en_US': {
+      'app_name': 'Dokan',
+    },
+    'bn_BD': {
+      'app_name': 'দোকান',
+    }
+  };
+}
---
+++ lib/localization/localization_constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Locale LOCALE_ENGLISH = Locale('en', 'US');
+const Locale LOCALE_BANGLA = Locale('bn', 'BD');
+
+const STR_APP_NAME = 'app_name';
