8c2752b808b2638610b29d2ca1e7916a7b510fe1
shafiul <riddlersajib@gmail.com>
2024-06-01 18:14:55 +0600

    User model class

---
+++ lib/models/user.dart
@@ -0,0 +1,27 @@
+class User {
+
+  int? id;
+  String? username;
+  String? email;
+  String? password;
+  String? name;
+  String? displayName;
+  String? accessToken;
+
+  User({this.id, this.username, this.email, this.password, this.name,
+    this.displayName, this.accessToken});
+
+  User.fromJson(Map<String, dynamic> json) {
+    id = json['id'] ?? null;
+    email = json['user_email'] ?? '';
+    name = json['user_nicename'] ?? '';
+    displayName = json['user_display_name'] ?? '';
+    accessToken = json['token'] ?? '';
+  }
+
+  toJson() => {
+    'username': username ?? '',
+    'email': email ?? '',
+    'password': password ?? '',
+  };
+}
