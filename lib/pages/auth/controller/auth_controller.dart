6cf27da7a28c7af6231400bdf8102f991e0d68c3
shafiul <riddlersajib@gmail.com>
2024-06-01 17:21:26 +0600

    User signup with validation check

---
+++ lib/pages/auth/controller/auth_controller.dart
@@ -0,0 +1,57 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../../barrels/localizations.dart';
+import '../../../barrels/models.dart';
+import '../../../barrels/repositories.dart';
+import '../../../barrels/utils.dart';
+
+class AuthController extends GetxController {
+
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  final _authRepo = getIt<AuthRepository>();
+
+  void validateSignupForm(User user) {
+
+    if(formKey.currentState!.validate()) {
+      _signup(user);
+    }
+  }
+
+  Future<void> _signup(User user) async {
+
+    try {
+      var response = await _authRepo.createUserAccount(user);
+
+      var jsonData = json.decode(response.body);
+
+      if((response.statusCode == HttpStatus.ok || response.statusCode == HttpStatus.created || response.statusCode == HttpStatus.accepted)
+        && jsonData['code'] == HttpStatus.ok) {
+
+        _showSnackBar(STR_SUCCESS.tr, jsonData['message']);
+        return;
+      }
+
+      _showSnackBar(STR_FAILED.tr, jsonData['message'], isErrorMsg: true);
+
+    } on AppException catch(error) {
+      _showSnackBar(STR_FAILED.tr, error.message!, isErrorMsg: true);
+
+    } catch(error) {
+      _showSnackBar(STR_FAILED.tr, STR_UNKNOWN_ERROR.tr, isErrorMsg: true);
+    }
+  }
+
+  void _showSnackBar(String title, message, {bool isErrorMsg = false}) {
+
+    Get.snackbar(title, message,
+      margin: EdgeInsets.all(15),
+      duration: const Duration(milliseconds: 3500),
+      colorText: isErrorMsg ? Colors.white : null,
+      backgroundColor: isErrorMsg ? Colors.redAccent : null,
+    );
+  }
+}
