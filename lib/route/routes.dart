bc6ae45de704f85c6d3187f07e005a7a5142bf9e
shafiul <riddlersajib@gmail.com>
2024-06-01 15:10:15 +0600

    Login and signup routes

--- lib/route/routes.dart
+++ lib/route/routes.dart
@@ -1,5 +1,12 @@
 import 'package:get/get.dart';
 
-const String ROUTE_INITIAL = '/';
+import '../barrels/pages.dart';
 
-final appPages = <GetPage>[];
+const String ROUTE_INITIAL = '/';
+const String ROUTE_SIGNUP = '/signup';
+
+final appPages = <GetPage>[
+
+  GetPage(name: ROUTE_INITIAL, page: () => Login()),
+  GetPage(name: ROUTE_SIGNUP, page: () => Signup()),
+];
