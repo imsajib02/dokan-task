8c01af1b88e0e39de7976ad01de5c972ff602898
shafiul <riddlersajib@gmail.com>
2024-06-01 18:12:46 +0600

    User login

--- lib/repositories/auth_repository.dart
+++ lib/repositories/auth_repository.dart
@@ -57,4 +57,49 @@ class AuthRepository {
       throw BadRequestException(message: STR_UNKNOWN_ERROR.tr);
     }
   }
+
+  Future<http.Response> signInUser(User user) async {
+
+    var result = await Connectivity().checkConnectivity();
+
+    if(result != ConnectivityResult.mobile && result != ConnectivityResult.wifi)
+      throw NoInternetException(message: STR_NO_INTERNET.tr);
+
+    var networkStatus = await InternetConnectionChecker().isHostReachable(options);
+
+    if(!networkStatus.isSuccess)
+      throw BadConnectionException(message: STR_INACTIVE_CONNECTION.tr);
+
+    showProgressIndicator();
+
+    var client = http.Client();
+
+    String url = dotenv.env['API_URL']! + dotenv.env['SIGNIN']!;
+
+    try {
+      var response = await client.post(Uri.parse(url),
+        body: user.toJson(),
+        headers: {'Content-Type' : 'application/x-www-form-urlencoded', 'Accept' : 'application/json'},
+      ).timeout(Duration(seconds: timeoutSeconds));
+
+      client.close();
+      Get.back(closeOverlays: true);
+
+      return response;
+
+    } on TimeoutException {
+
+      client.close();
+      Get.back(closeOverlays: true);
+
+      throw ConnectionTimedOutException(message: STR_TIMED_OUT.tr);
+
+    } catch(error) {
+
+      client.close();
+      Get.back(closeOverlays: true);
+
+      throw BadRequestException(message: STR_UNKNOWN_ERROR.tr);
+    }
+  }
 }
