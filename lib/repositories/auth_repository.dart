48039096c414040cf3d0341303f335227e515470
shafiul <riddlersajib@gmail.com>
2024-06-01 16:24:30 +0600

    Auth repository

---
+++ lib/repositories/auth_repository.dart
@@ -0,0 +1,15 @@
+import 'dart:async';
+
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+
+import '../barrels/localizations.dart';
+import '../barrels/models.dart';
+import '../barrels/utils.dart';
+
+class AuthRepository {
+
+}
