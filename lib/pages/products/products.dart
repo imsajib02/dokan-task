14155fc2a52500b476cc1b42d84eff57b3a5dee3
imsajib02 <shafiul.imsajib@gmail.com>
2024-06-02 16:34:41 +0600

    Products with grid view

--- lib/pages/products/products.dart
+++ lib/pages/products/products.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../../barrels/utils.dart';
 import '../../barrels/localizations.dart';
 import '../../barrels/widgets.dart';
+import 'controller/product_controller.dart';
 
 class Products extends StatelessWidget {
+
+  final _controller = Get.put(ProductController());
 
   Products({Key? key}) : super(key: key);
 
@@ -22,14 +26,37 @@ class Products extends StatelessWidget {
             ),
           ],
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
+        body: Obx(() => SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
 
+                20.h,
 
-            ],
+                GridView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: _controller.productList.length,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 20,
+                    mainAxisSpacing: 20,
+                    childAspectRatio: .6,
+                  ),
+                  itemBuilder: (context, index) {
+
+                    return ProductItem(
+                      product: _controller.productList[index],
+                    );
+                  },
+                ),
+
+                50.h,
+              ],
+            ),
           ),
-        ),
+        )),
       ),
     );
   }
