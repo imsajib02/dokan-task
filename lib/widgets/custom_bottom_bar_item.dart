06d75e86889f23b0c7ddf79e1078b2aff5b85b82
imsajib02 <shafiul.imsajib@gmail.com>
2024-06-02 12:50:01 +0600

    Home page bottom bar navigation item widget

---
+++ lib/widgets/custom_bottom_bar_item.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class CustomBottomBarItem extends StatelessWidget {
+
+  final String imagePath;
+  final int indexPosition;
+  final int activeIndex;
+  final Function(int) onTap;
+
+  const CustomBottomBarItem({Key? key, required this.imagePath, required this.indexPosition, required this.onTap, required this.activeIndex}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: () => onTap(indexPosition),
+      child: Center(
+        child: Image.asset(imagePath,
+          height: 22,
+          width: 22,
+          color: Color(0xFF6E7FAA),
+          fit: BoxFit.contain,
+        ),
+      ),
+    );
+  }
+}
