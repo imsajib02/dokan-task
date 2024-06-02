5fd7e0fb819b7f479b19efc48a5da8091f0afeb6
imsajib02 <shafiul.imsajib@gmail.com>
2024-06-02 13:01:00 +0600

    Home page route added

--- lib/route/routes.dart
+++ lib/route/routes.dart
@@ -4,9 +4,11 @@ import '../barrels/pages.dart';
 
 const String ROUTE_INITIAL = '/';
 const String ROUTE_SIGNUP = '/signup';
+const String ROUTE_HOME = '/home';
 
 final appPages = <GetPage>[
 
   GetPage(name: ROUTE_INITIAL, page: () => Login()),
   GetPage(name: ROUTE_SIGNUP, page: () => Signup()),
+  GetPage(name: ROUTE_HOME, page: () => Home()),
 ];
