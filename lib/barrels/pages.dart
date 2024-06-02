092142d7e6a2b02c70ce61a0385104f69d475cbe
imsajib02 <shafiul.imsajib@gmail.com>
2024-06-02 17:07:26 +0600

    Pages barrel import updated

--- lib/barrels/pages.dart
+++ lib/barrels/pages.dart
@@ -1,2 +1,5 @@
 export '../pages/auth/login.dart';
 export '../pages/auth/sign_up.dart';
+export '../pages/home/home.dart';
+export '../pages/products/products.dart';
+export '../pages/account/my_account.dart';
