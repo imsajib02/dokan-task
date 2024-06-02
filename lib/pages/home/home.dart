4a27ccfbf09473f58bbdfd44cc992bd76eef3aa3
imsajib02 <shafiul.imsajib@gmail.com>
2024-06-02 13:37:34 +0600

    Wrap view inside safe area

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -20,90 +20,92 @@ class Home extends StatelessWidget {
         SystemNavigator.pop();
         return Future(() => false);
       },
-      child: Obx(() => Scaffold(
-        floatingActionButton: Visibility(
-          visible: MediaQuery.of(context).viewInsets.bottom == 0,
-          child: Material(
-            elevation: 2,
-            shape: CircleBorder(),
-            child: Container(
-              height: 56,
-              width: 56,
-              padding: EdgeInsets.symmetric(vertical: 14.65, horizontal: 14.44),
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                gradient: LinearGradient(
-                  begin: const FractionalOffset(0.0, 0.0),
-                  end: const FractionalOffset(0.5, 0.0),
-                  stops: [0.0, 1.0],
-                  tileMode: TileMode.clamp,
-                  colors: <Color>[
-                    Color(0xFFFF679B),
-                    Color(0xFFFF7B51),
-                  ],
+      child: Obx(() => SafeArea(
+        child: Scaffold(
+          floatingActionButton: Visibility(
+            visible: MediaQuery.of(context).viewInsets.bottom == 0,
+            child: Material(
+              elevation: 2,
+              shape: CircleBorder(),
+              child: Container(
+                height: 56,
+                width: 56,
+                padding: EdgeInsets.symmetric(vertical: 14.65, horizontal: 14.44),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  gradient: LinearGradient(
+                    begin: const FractionalOffset(0.0, 0.0),
+                    end: const FractionalOffset(0.5, 0.0),
+                    stops: [0.0, 1.0],
+                    tileMode: TileMode.clamp,
+                    colors: <Color>[
+                      Color(0xFFFF679B),
+                      Color(0xFFFF7B51),
+                    ],
+                  ),
                 ),
-              ),
-              child: FittedBox(
-                child: Icon(Icons.search, color: Colors.white),
+                child: FittedBox(
+                  child: Icon(Icons.search, color: Colors.white),
+                ),
               ),
             ),
           ),
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-        bottomNavigationBar: AnimatedBottomNavigationBar.builder(
-          itemCount: 4,
-          elevation: 4,
-          backgroundColor: Colors.white,
-          activeIndex: _homeController.activeIndex.value,
-          splashColor: Theme.of(context).primaryColor,
-          splashSpeedInMilliseconds: 300,
-          notchSmoothness: NotchSmoothness.defaultEdge,
-          gapLocation: GapLocation.center,
-          rightCornerRadius: 15,
-          leftCornerRadius: 15,
-          onTap: (index) {},
-          tabBuilder: (int index, bool isActive) {
+          floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+          bottomNavigationBar: AnimatedBottomNavigationBar.builder(
+            itemCount: 4,
+            elevation: 4,
+            backgroundColor: Colors.white,
+            activeIndex: _homeController.activeIndex.value,
+            splashColor: Theme.of(context).primaryColor,
+            splashSpeedInMilliseconds: 300,
+            notchSmoothness: NotchSmoothness.defaultEdge,
+            gapLocation: GapLocation.center,
+            rightCornerRadius: 15,
+            leftCornerRadius: 15,
+            onTap: (index) {},
+            tabBuilder: (int index, bool isActive) {
 
-            return IndexedStack(
-              index: index,
-              children: [
+              return IndexedStack(
+                index: index,
+                children: [
 
-                CustomBottomBarItem(
-                  imagePath: 'assets/images/home.png',
-                  indexPosition: 0,
-                  activeIndex: _homeController.activeIndex.value,
-                  onTap: (index) => _homeController.updateActiveIndex(index),
-                ),
+                  CustomBottomBarItem(
+                    imagePath: 'assets/images/home.png',
+                    indexPosition: 0,
+                    activeIndex: _homeController.activeIndex.value,
+                    onTap: (index) => _homeController.updateActiveIndex(index),
+                  ),
 
-                CustomBottomBarItem(
-                  imagePath: 'assets/images/categories.png',
-                  indexPosition: 1,
-                  activeIndex: _homeController.activeIndex.value,
-                  onTap: (index) => _homeController.updateActiveIndex(index),
-                ),
+                  CustomBottomBarItem(
+                    imagePath: 'assets/images/categories.png',
+                    indexPosition: 1,
+                    activeIndex: _homeController.activeIndex.value,
+                    onTap: (index) => _homeController.updateActiveIndex(index),
+                  ),
 
-                CustomBottomBarItem(
-                  imagePath: 'assets/images/cart.png',
-                  indexPosition: 2,
-                  activeIndex: _homeController.activeIndex.value,
-                  onTap: (index) => _homeController.updateActiveIndex(index),
-                ),
+                  CustomBottomBarItem(
+                    imagePath: 'assets/images/cart.png',
+                    indexPosition: 2,
+                    activeIndex: _homeController.activeIndex.value,
+                    onTap: (index) => _homeController.updateActiveIndex(index),
+                  ),
 
-                CustomBottomBarItem(
-                  imagePath: 'assets/images/account.png',
-                  indexPosition: 3,
-                  activeIndex: _homeController.activeIndex.value,
-                  onTap: (index) => _homeController.updateActiveIndex(index),
-                ),
-              ],
-            );
-          },
-        ),
-        body: Builder(
-          builder: (BuildContext context) {
+                  CustomBottomBarItem(
+                    imagePath: 'assets/images/account.png',
+                    indexPosition: 3,
+                    activeIndex: _homeController.activeIndex.value,
+                    onTap: (index) => _homeController.updateActiveIndex(index),
+                  ),
+                ],
+              );
+            },
+          ),
+          body: Builder(
+            builder: (BuildContext context) {
 
-            return Container();
-          },
+              return Container();
+            },
+          ),
         ),
       )),
     );
