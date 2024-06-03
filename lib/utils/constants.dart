caa46903dd9ea1cf43eae7c3acac61a046a417d7
imsajib02 <shafiul.imsajib@gmail.com>
2024-06-03 10:40:24 +0600

    Password min length const value

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -3,5 +3,6 @@ import 'dart:io';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
 
 const int timeoutSeconds = 40;
+const int minPasswordLength = 4;
 
 final AddressCheckOptions options = AddressCheckOptions(InternetAddress('8.8.8.8'), port: 53, timeout: Duration(seconds: 2));
