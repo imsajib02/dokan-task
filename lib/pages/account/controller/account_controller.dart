269344a4565ab72c697a7969b782f04feb0e3e53
imsajib02 <shafiul.imsajib@gmail.com>
2024-06-03 13:53:28 +0600

    Account tile expansion status managing

--- lib/pages/account/controller/account_controller.dart
+++ lib/pages/account/controller/account_controller.dart
@@ -1,5 +1,8 @@
 import 'package:get/get.dart';
 
 class AccountController extends GetxController {
-  
+
+  var isExpanded = [false, false, false, false].obs;
+
+  void toggleExpansionStatus(int position) => isExpanded[position] = !isExpanded[position];
 }
