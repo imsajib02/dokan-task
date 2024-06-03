05e5446690cad02334ff64d5cd7dacf88d9e14e1
imsajib02 <shafiul.imsajib@gmail.com>
2024-06-03 15:35:31 +0600

    Widgets barrel imports updated

--- lib/barrels/widgets.dart
+++ lib/barrels/widgets.dart
@@ -4,3 +4,6 @@ export '../widgets/social_button.dart';
 export '../widgets/custom_bottom_bar_item.dart';
 export '../widgets/custom_app_bar.dart';
 export '../widgets/product_item.dart';
+export '../widgets/profile_expansion_tile.dart';
+export '../widgets/account_update_field.dart';
+export '../widgets/account_update_button.dart';
