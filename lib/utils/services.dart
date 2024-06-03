4bd56abb066ce79930c8460fbed0aa89a1d6d207
imsajib02 <shafiul.imsajib@gmail.com>
2024-06-03 10:57:53 +0600

    Register storage pref class as singleton

--- lib/utils/services.dart
+++ lib/utils/services.dart
@@ -1,9 +1,11 @@
 import 'package:get_it/get_it.dart';
 
 import '../barrels/repositories.dart';
+import '../barrels/utils.dart';
 
 final getIt = GetIt.instance;
 
 void registerServices() {
+  getIt.registerLazySingleton<MyPref>(() => MyPref());
   getIt.registerLazySingleton<AuthRepository>(() => AuthRepository());
 }
