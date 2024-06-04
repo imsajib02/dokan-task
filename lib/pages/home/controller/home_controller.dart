f14bf38a85ea3885f33433fb98a1fe42a26652bb
imsajib02 <shafiul.imsajib@gmail.com>
2024-06-04 10:55:00 +0600

    On controller start set first name and last name

--- lib/pages/home/controller/home_controller.dart
+++ lib/pages/home/controller/home_controller.dart
@@ -1,8 +1,27 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
+import '../../auth/controller/auth_controller.dart';
 
 class HomeController extends GetxController {
 
   var activeIndex = 0.obs;
 
+  final firstNameController = TextEditingController();
+  final lastNameController = TextEditingController();
+
+  final _authController = Get.find<AuthController>();
+
+  @override
+  void onInit() {
+    setAuthUserAccountName();
+    super.onInit();
+  }
+
   void updateActiveIndex(int index) => activeIndex.value = index;
+
+  void setAuthUserAccountName() {
+    firstNameController.text = _authController.authUser.value!.firstName!;
+    lastNameController.text = _authController.authUser.value!.lastName!;
+  }
 }
