4c24fedc54d7238af7e9fddd865c211075697c8e
imsajib02 <shafiul.imsajib@gmail.com>
2024-06-04 09:24:55 +0600

    Option type property added

--- lib/models/filter_option.dart
+++ lib/models/filter_option.dart
@@ -1,15 +1,19 @@
+import '../barrels/utils.dart';
+
 class FilterOption {
 
   final String title;
   final bool isChecked;
+  final filterTypes type;
 
-  FilterOption({required this.title, required this.isChecked});
+  FilterOption({required this.title, required this.isChecked, required this.type});
 
-  FilterOption copyWith({String? title, bool? isChecked}) {
+  FilterOption copyWith({String? title, bool? isChecked, filterTypes? type}) {
 
     return FilterOption(
       title: title ?? this.title,
       isChecked: isChecked ?? this.isChecked,
+      type: type ?? this.type,
     );
   }
 }
