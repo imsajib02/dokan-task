12dc20541aaac4263278b32fae17c8ce7b5beb7b
imsajib02 <shafiul.imsajib@gmail.com>
2024-06-04 10:18:07 +0600

    Use colors from resource file

--- lib/widgets/overlay_widgets.dart
+++ lib/widgets/overlay_widgets.dart
@@ -2,14 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:lottie/lottie.dart';
 
+import '../barrels/resources.dart';
+
 void showProgressIndicator() => Get.dialog(
   Center(
     child: Container(
       height: 120,
       width: 120,
       decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(16),
+        color: kWhite,
+        borderRadius: BorderRadius.circular(16),
       ),
       child: Lottie.asset('assets/animations/sending.json'),
     ),
@@ -22,7 +24,7 @@ void showSnackBar(String title, message, {bool isErrorMsg = false}) {
   Get.snackbar(title, message,
     margin: const EdgeInsets.all(15),
     duration: const Duration(milliseconds: 3500),
-    colorText: isErrorMsg ? Colors.white : null,
-    backgroundColor: isErrorMsg ? Colors.redAccent : null,
+    colorText: isErrorMsg ? kWhite : null,
+    backgroundColor: isErrorMsg ? kRedOrange : null,
   );
 }
