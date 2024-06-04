48e5f203b03196468f17ed609770a12635a58e77
imsajib02 <shafiul.imsajib@gmail.com>
2024-06-04 10:05:46 +0600

    Progress dialog barrel import removed

--- lib/barrels/utils.dart
+++ lib/barrels/utils.dart
@@ -2,5 +2,4 @@ export '../utils/custom_extensions.dart';
 export '../utils/app_exceptions.dart';
 export '../utils/constants.dart';
 export '../utils/services.dart';
-export '../utils/progress_indicator.dart';
 export '../utils/my_pref.dart';
